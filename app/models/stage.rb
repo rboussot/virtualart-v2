9c07683aaddde7d99596b209e8ecaa9aa620b71f
Romain Boussot <rboussot@gmail.com>
2018-02-20 11:02:18 +0100

    correction de l'admin pour les stages

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -3,10 +3,6 @@ class Stage < ApplicationRecord
   has_many :users_stages
   has_many :users, through: :users_stages
 
-  def name
-    stagetype.title+" - "+timeslot
-  end
-
   rails_admin do
     list do
       field :stagetype
