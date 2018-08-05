23d187ebd0a505c88be0cbef1a0df464947b4c41
Romain Boussot <rboussot@gmail.com>
2018-08-05 17:47:24 +0200

    variable d'environnement pour le code API de mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "https://virtualart-v2.herokuapp.com" }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: 'ENV["MAILGUN_API_KEY"]',
+    api_key: ENV["MAILGUN_API_KEY"],
     domain:  'mg.virtualart.pro'
   }
   # Settings specified here will take precedence over those in config/application.rb.
